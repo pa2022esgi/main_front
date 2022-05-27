03917ebd600c819a99ad72549e5a50e95259f59d
Charles <charlescretois@gmail.com>
2022-05-27 20:36:45 +0200

    Creation User Model (faux)

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,29 @@
+const userSchema = new Schema({
+  login: {
+    type: Schema.Types.String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: Schema.Types.String,
+    required: true
+  },
+  name: {
+    type: Schema.Types.String,
+    required: false
+  },
+  type: {
+    type: Schema.Types.Number,
+    ref: "UserType"
+  },
+  address: {
+    type: Schema.Types.String
+  },
+  phone: {
+    type: Schema.Types.Number,
+  },
+}, {
+  collection: "users",
+  timestamps: true,
+  versionKey: false
+});
